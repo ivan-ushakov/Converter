14dfdba78ba977928944139284c2028f588844ce
 Ivan Ushakov <ivan.ushakov@gmail.com>
2018-07-30 13:01:14 +0300

    setup

--- Converter/AppDelegate.swift
+++ Converter/AppDelegate.swift
@@ -11,11 +11,16 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = UIColor.clear
+
+        let navigationController = UINavigationController(rootViewController: ConverterViewController())
+        navigationController.navigationBar.isHidden = true
+
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+
         return true
     }
 
@@ -40,7 +45,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
---
+++ Converter/ConverterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ConverterViewController.swift
+//  Converter
+//
+//  Created by  Ivan Ushakov on 30/07/2018.
+//  Copyright © 2018  Ivan Ushakov. All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
